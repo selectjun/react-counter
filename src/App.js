6316b89d1103721d9518a5412f81c7391cab3ff4
selectjun <selectjun@gmail.com>
2021-06-25 13:01:29 +0900

    카운터 기본 기능 추가.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "INCREASE":
+      return state + 1;
+    case "DESCREASE":
+      return state - 1;
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, 0);
+
+  const onIncrease = () => {
+    dispatch({ type: "INCREASE" });
+  };
+
+  const onDescrease = () => {
+    dispatch({ type: "DESCREASE" });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="count">{count}</h1>
+      <div className="button-group">
+        <button onClick={onDescrease}>-</button>
+        <button onClick={onIncrease}>+</button>
+      </div>
+
     </div>
   );
 }
